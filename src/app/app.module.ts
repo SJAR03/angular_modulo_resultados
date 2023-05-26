682c1ff10b93c610024ae37b06aa225fff9505ea
eduardo <eduardojose13579@gmail.com>
2023-05-25 23:07:43 -0600

    el diablo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ResultsModule } from './results/results.module';
 import { SharedModule } from "./shared/shared.module";
+import { ListaComponent } from './orden/lista/lista.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaComponent
   ],
   imports: [
     BrowserModule,
