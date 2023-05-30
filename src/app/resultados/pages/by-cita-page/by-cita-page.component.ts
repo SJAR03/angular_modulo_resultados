e5949c55e07c07a226ba227a5f3821b9f200191a
eduardo <eduardojose13579@gmail.com>
2023-05-30 05:05:25 -0600

    filtro de resultado tipoOrden i2udbxnuicn

--- src/app/resultados/pages/by-cita-page/by-cita-page.component.ts
+++ src/app/resultados/pages/by-cita-page/by-cita-page.component.ts
@@ -2,28 +2,41 @@ import { Component } from '@angular/core';
 import { Resultados } from '../../interfaces/results';
 import { ResultadosService } from '../../services/resultados.service';
 
-type Cita = 'Especializados'|'Generales'|'Diagnóstico';
+type Cita = 'Especializados' | 'Generales' | 'Diagnóstico';
 
 @Component({
   selector: 'resultados-by-cita-page',
   templateUrl: './by-cita-page.component.html',
-  styles: [
-  ]
+  styles: []
 })
 export class ByCitaPageComponent {
 
   public resultados: Resultados[] = [];
-  public citas: Cita[] = ['Especializados','Generales','Diagnóstico'];
+  public citas: Cita[] = ['Especializados', 'Generales', 'Diagnóstico'];
   public selectedCita?: Cita;
 
-  constructor(private resultadosService: ResultadosService){}
+  constructor(private resultadosService: ResultadosService) { }
 
-  searchByCita(cita:Cita):void{
-    this.selectedCita = cita;
-    this.resultadosService.searchByCita(cita)
-    .subscribe(resultados => {
-      this.resultados = resultados
-    });
-
+  public searchByCita(): void {
+    this.resultadosService.searchByCita(1).subscribe(
+      resultados => {
+        this.resultados = resultados;
+        console.log(resultados);
+      },
+      error => {
+        console.error(error);
+      }
+    );
+  }
+  public todo(): void {
+    this.resultadosService.getResultadosRequest().subscribe(
+      resultados => {
+        this.resultados = resultados;
+        console.log(resultados);
+      },
+      error => {
+        console.error(error);
+      }
+    );
   }
 }
--- src/app/resultados/services/resultados.service.ts
+++ src/app/resultados/services/resultados.service.ts
@@ -1,43 +1,72 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, map, of, pipe } from 'rxjs';
-import { Resultados } from '../interfaces/results';
+import { Observable, catchError, map, of, switchMap } from 'rxjs';
+import { Resultados, Orden } from '../interfaces/results';
 
 @Injectable({providedIn: 'root'})
 export class ResultadosService {
 
-private apiUrl:string = 'http://localhost:8082/api/resultados';
+  private apiUrlResultados: string = 'http://localhost:8086/api/resultados';
+  private apiUrlOrden: string = 'http://localhost:8084/api/ordenes';
 
   constructor(private http: HttpClient) { }
 
-  private getResultadosRequest(url: string): Observable<Resultados[]>{
+  getResultadosRequest(url= "http://localhost:8086/api/resultados"): Observable<Resultados[]> {
+    return this.http.get<Resultados[]>(url)
+      .pipe(
+        catchError(() => of([]))
+      );
+  }
+  searchResultadosByAlphaCode(code: string): Observable<Resultados | null>{
+    const url = `${this.apiUrlResultados}/alpha/${code}`;
    return this.http.get<Resultados[]>(url)
-     .pipe(
-     catchError( () => of([]))
+   .pipe(
+      map((resultados: string | any[]) => resultados.length > 0 ? resultados[0]: null),
+      catchError( () => of(null))
+    );
+   }
+   
+   searchByCita(cita: number): Observable<Resultados[]> {
+    const urlOrdenes = `${this.apiUrlOrden}/tipo/${cita}`; // URL para obtener las órdenes filtradas por IdTipoOrden
+  
+    return this.http.get<Orden[]>(urlOrdenes).pipe(
+      switchMap((ordenes: Orden[]) => {
+        // Obtener los IdOrden de las órdenes filtradas
+        const idsOrden = ordenes.map(orden => orden.idOrden);
+  
+        console.log(idsOrden);
+  
+        if (idsOrden.length === 0) {
+          // No hay órdenes con el IdTipoOrden dado, devolver un arreglo vacío
+          return of([]);
+        }
+  
+        // Verificar si todos los IdOrden son válidos
+        if (idsOrden.some(id => id === undefined || id === null)) {
+          console.error('Algunos objetos de orden no tienen la propiedad IdOrden válida');
+          return of([]);
+        }
+  
+        // Construir la URL para obtener los resultados por los IdOrden filtrados
+        const urlResultadosFiltrados = `${this.apiUrlResultados}/ordenes/${idsOrden.map(String).join(',')}`;
+        console.log(urlResultadosFiltrados);
+  
+        return this.http.get<Resultados[]>(urlResultadosFiltrados);
+      }),
+      catchError(() => of([]))
     );
   }
+  
+  
+  
 
-  searchResultadosByAlphaCode(code: string): Observable<Resultados | null>{
-   const url = `${this.apiUrl}/alpha/${code}`;
-  return this.http.get<Resultados[]>(url)
-  .pipe(
-     map(resultados => resultados.length > 0 ? resultados[0]: null),
-     catchError( () => of(null))
-   );
-  }
-
-   searchByCita( cita: string): Observable<Resultados[]>{
-    const url = `${this.apiUrl}/cita/${cita}`;
+  searchByEmergencia( emergencia: string): Observable<Resultados[]>{
+    const url = `${this.apiUrlOrden}/emergencia/${emergencia}`;
    return this.getResultadosRequest(url);
    }
-
-   searchByEmergencia( emergencia: string): Observable<Resultados[]>{
-    const url = `${this.apiUrl}/emergencia/${emergencia}`;
-   return this.getResultadosRequest(url);
-   }
-
    searchByRutina( rutina: string): Observable<Resultados[]>{
-    const url = `${this.apiUrl}/rutina/${rutina}`;
+    const url = `${this.apiUrlOrden}/rutina/${rutina}`;
    return this.getResultadosRequest(url);
    }
  }
+
