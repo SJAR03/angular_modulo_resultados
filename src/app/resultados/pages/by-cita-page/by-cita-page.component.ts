0cd28c1807bdea6a4509044e07a15fec74d94c8d
Asbell06 <asbel.palma11970@est.uca.edu.ni>
2023-05-31 14:50:10 -0600

    filtrado por área lab = listo

--- src/app/resultados/pages/by-cita-page/by-cita-page.component.ts
+++ src/app/resultados/pages/by-cita-page/by-cita-page.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 import { Resultados } from '../../interfaces/results';
 import { ResultadosService } from '../../services/resultados.service';
 
-type AreaExamen = 'Analisis clinico' | 'Hematologia' | 'Microbiologia' | 'Inmunologia' |
- 'Serologia' | 'Parasitologia' | 'Inmunohematología y banco de sangre' | 'Genética humana y diagnóstico molecular';
+type AreaExamen = 'Análisis clínico' | 'Hematología' | 'Microbiología' | 'Inmunología' |
+ 'Serología' | 'Parasitología' | 'Inmunohematología y banco de sangre' | 'Genética humana y diagnóstico molecular';
 
 @Component({
   selector: 'resultados-by-cita-page',
@@ -13,8 +13,8 @@ type AreaExamen = 'Analisis clinico' | 'Hematologia' | 'Microbiologia' | 'Inmuno
 export class ByCitaPageComponent {
 
   public resultados: Resultados[] = [];
-  public citas: AreaExamen[] = ['Analisis clinico', 'Hematologia', 'Microbiologia', 
-  'Inmunologia', 'Serologia', 'Parasitologia', 'Inmunohematología y banco de sangre', 'Genética humana y diagnóstico molecular'];
+  public citas: AreaExamen[] = ['Análisis clínico', 'Hematología', 'Microbiología', 
+  'Inmunología', 'Serología', 'Parasitología', 'Inmunohematología y banco de sangre', 'Genética humana y diagnóstico molecular'];
   public selectedExamen?: AreaExamen;
   public selectedCategoriaExamen?: number;
 
@@ -23,22 +23,22 @@ export class ByCitaPageComponent {
   public searchByTipoOrden(): void {
     let idAreaExamen: number | undefined = undefined;
   
-    if (this.selectedExamen === 'Analisis clinico') {
-      idAreaExamen = 1; // Asigna el valor 1 para 'Especializados'
-    } else if (this.selectedExamen === 'Hematologia') {
-      idAreaExamen = 2; // Asigna el valor 2 para 'Generales'
-    } else if (this.selectedExamen === 'Microbiologia') {
-      idAreaExamen = 3; // Asigna el valor 3 para 'Diagnóstico'
-    }else if (this.selectedExamen === 'Inmunologia') {
-      idAreaExamen = 4; // Asigna el valor 3 para 'Diagnóstico'
-    }else if (this.selectedExamen === 'Serologia') {
-      idAreaExamen = 5; // Asigna el valor 3 para 'Diagnóstico'
-    }else if (this.selectedExamen === 'Parasitologia') {
-      idAreaExamen = 6; // Asigna el valor 3 para 'Diagnóstico'
+    if (this.selectedExamen === 'Análisis clínico') {
+      idAreaExamen = 1; // Asigna el valor 1 para 'Análisis Clínico'
+    } else if (this.selectedExamen === 'Hematología') {
+      idAreaExamen = 2; // Asigna el valor 2 para 'Hematología'
+    } else if (this.selectedExamen === 'Microbiología') {
+      idAreaExamen = 3; // Asigna el valor 3 para 'Microbiología'
+    }else if (this.selectedExamen === 'Inmunología') {
+      idAreaExamen = 4; // Asigna el valor 3 para 'Inmunología'
+    }else if (this.selectedExamen === 'Serología') {
+      idAreaExamen = 5; // Asigna el valor 3 para 'Serología'
+    }else if (this.selectedExamen === 'Parasitología') {
+      idAreaExamen = 6; // Asigna el valor 3 para 'Parasitologia'
     }else if (this.selectedExamen === 'Inmunohematología y banco de sangre') {
-      idAreaExamen = 7; // Asigna el valor 3 para 'Diagnóstico'
+      idAreaExamen = 7; // Asigna el valor 3 para 'Inmunohematología y banco de sangre'
     }else if (this.selectedExamen === 'Genética humana y diagnóstico molecular') {
-      idAreaExamen = 8; // Asigna el valor 3 para 'Diagnóstico'
+      idAreaExamen = 8; // Asigna el valor 3 para 'Genética humana y diagnóstico molecular'
     }
   
     if (idAreaExamen !== undefined) {
--- src/app/resultados/pages/by-rutina-page/by-rutina-page.component.ts
+++ src/app/resultados/pages/by-rutina-page/by-rutina-page.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { Resultados } from '../../interfaces/results';
 import { ResultadosService } from '../../services/resultados.service';
 
-type TipoOrden = 'Especializados' | 'Generales' | 'Diagnóstico';
+type AreaExamen = 'Análisis clínico' | 'Hematología' | 'Microbiología' | 'Inmunología' |
+ 'Serología' | 'Parasitología' | 'Inmunohematología y banco de sangre' | 'Genética humana y diagnóstico molecular';
 
 @Component({
   selector: 'resultados-by-rutina-page',
@@ -12,31 +13,46 @@ type TipoOrden = 'Especializados' | 'Generales' | 'Diagnóstico';
 export class ByRutinaPageComponent {
 
   public resultados: Resultados[] = [];
-  public rutinas: TipoOrden[] = ['Especializados', 'Generales', 'Diagnóstico'];
-  public selectedRutina?: TipoOrden;
+  public rutinas: AreaExamen[] = ['Análisis clínico', 'Hematología', 'Microbiología', 
+  'Inmunología', 'Serología', 'Parasitología', 'Inmunohematología y banco de sangre', 'Genética humana y diagnóstico molecular'];
+  public selectedExamen?: AreaExamen;
+  public selectedCategoriaExamen?: number;
 
   constructor(private resultadosService: ResultadosService) { }
 
   public searchByTipoOrden(): void {
-    this.resultadosService.searchByTipoOrden(3,2).subscribe(
-      resultados => {
-        this.resultados = resultados;
-        console.log(resultados);
-      },
-      error => {
-        console.error(error);
-      }
-    );
+    let idAreaExamen: number | undefined = undefined;
+  
+    if (this.selectedExamen === 'Análisis clínico') {
+      idAreaExamen = 1; // Asigna el valor 1 para 'Análisis Clínico'
+    } else if (this.selectedExamen === 'Hematología') {
+      idAreaExamen = 2; // Asigna el valor 2 para 'Hematología'
+    } else if (this.selectedExamen === 'Microbiología') {
+      idAreaExamen = 3; // Asigna el valor 3 para 'Microbiología'
+    }else if (this.selectedExamen === 'Inmunología') {
+      idAreaExamen = 4; // Asigna el valor 3 para 'Inmunología'
+    }else if (this.selectedExamen === 'Serología') {
+      idAreaExamen = 5; // Asigna el valor 3 para 'Serología'
+    }else if (this.selectedExamen === 'Parasitología') {
+      idAreaExamen = 6; // Asigna el valor 3 para 'Parasitologia'
+    }else if (this.selectedExamen === 'Inmunohematología y banco de sangre') {
+      idAreaExamen = 7; // Asigna el valor 3 para 'Inmunohematología y banco de sangre'
+    }else if (this.selectedExamen === 'Genética humana y diagnóstico molecular') {
+      idAreaExamen = 8; // Asigna el valor 3 para 'Genética humana y diagnóstico molecular'
+    }
+  
+    if (idAreaExamen !== undefined) {
+      this.resultadosService.searchByTipoOrden(3, idAreaExamen).subscribe(
+        resultados => {
+          this.resultados = resultados;
+          console.log(resultados);
+        },
+        error => {
+          console.error(error);
+        }
+      );
+    }
   }
-  public todo(): void {
-    this.resultadosService.getResultadosRequest().subscribe(
-      resultados => {
-        this.resultados = resultados;
-        console.log(resultados);
-      },
-      error => {
-        console.error(error);
-      }
-    );
-  }
+  
 }
+
