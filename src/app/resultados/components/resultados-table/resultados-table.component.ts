3c015c69724cf3e3e6ae2fce2b2536435278ff79
eduardo <eduardojose13579@gmail.com>
2023-06-01 22:34:43 -0600

    read

--- src/app/resultados/components/resultados-table/resultados-table.component.ts
+++ src/app/resultados/components/resultados-table/resultados-table.component.ts
@@ -1,22 +1,22 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Examen, Orden, Resultados } from '../../interfaces/results'
+import { Component, Input } from '@angular/core';
+import { Examen, Orden, Resultados } from '../../interfaces/results';
 import { ResultadosService } from '../../services/resultados.service';
 
 @Component({
   selector: 'resultados-table',
   templateUrl: './resultados-table.component.html',
-  styles: [
-  ]
 })
-export class ResultadosTableComponent{
+export class ResultadosTableComponent {
   @Input()
   public resultados: Resultados[] = [];
   public ordenes: Orden[] = [];
   public examenes: Examen[] = [];
+  public popupData: Resultados | null = null;
+  public showPopup = false;
 
-  constructor(private resultadosService: ResultadosService) { }
+  constructor(private resultadosService: ResultadosService) {}
 
-  ngOnChanges() {
+  ngOnChanges(): void {
     if (this.resultados.length > 0) {
       const idsOrden = this.resultados.map(resultado => resultado.idOrden);
       const idsExamen = this.resultados.map(resultado => resultado.idExamen);
@@ -43,5 +43,18 @@ export class ResultadosTableComponent{
     const examen = this.examenes.find(examen => examen.idExamen === idExamen);
     return examen ? examen.descripcionCorta : '';
   }
-  
+
+  mostrarPopup(resultado: Resultados): void {
+    this.popupData = resultado;
+    this.showPopup = true;
+    console.log(this.popupData)
+  }
+
+  cerrarPopup(event: Event): void {
+    event.stopPropagation();
+    this.popupData = null;
+    this.showPopup = false;
+  }
 }
+
+
