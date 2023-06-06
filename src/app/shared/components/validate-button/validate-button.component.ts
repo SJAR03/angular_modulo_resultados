917efcef18d9b361ba3dbf24ce065a3a63df5417
eduardo <eduardojose13579@gmail.com>
2023-06-06 07:22:57 -0600

    ayuda

--- src/app/shared/components/validate-button/validate-button.component.ts
+++ src/app/shared/components/validate-button/validate-button.component.ts
@@ -1,25 +1,29 @@
-import { Component } from '@angular/core';
-import { ResultadosService} from 'src/app/resultados/services/resultados.service';
+import { Component, OnInit } from '@angular/core';
+import { ResultadosService } from 'src/app/resultados/services/resultados.service';
 import { Resultados, OrdenDetalle } from 'src/app/resultados/interfaces/results';
-import { catchError, distinct, forkJoin, map, of } from 'rxjs';
+import { catchError, forkJoin, map, of } from 'rxjs';
+
 @Component({
   selector: 'shared-validate-button',
   templateUrl: './validate-button.component.html',
   styleUrls: ['./validate-button.component.css']
 })
-export class ValidateButtonComponent {
+export class ValidateButtonComponent implements OnInit {
   public mostrarFormularioValidacion: boolean = false;
   public fechaSeleccionada: string | null = null;
   public idUsuarioSeleccionado: string | null = null;
   public idOrdenSeleccionada: number | null = null;
-  public examanesSinResultado: boolean = false;
+  public examenesSinResultado: boolean = false;
   public resultados: Resultados[] = [];
   public ordenesConResultados: number[] = [];
-  ordenesDetalle: OrdenDetalle[] = [];
-  ordenesConExamenesPendientes: number[] = [];
+  public ordenesDetalle: OrdenDetalle[] = [];
+  public ordenesConExamenesPendientes: number[] = [];
+  public mostrarBotonValidar: boolean = false;
+  public mostrarBotonCancelar: boolean = false;
+  public validacionCancelada: boolean = false;
 
   constructor(private resultadosService: ResultadosService) {}
-
+ 
   ngOnInit(): void {
     this.listarOrdenesDetalle();
   }
@@ -49,47 +53,118 @@ export class ValidateButtonComponent {
               self.findIndex((o) => o.idOrden === ordenDetalle.idOrden) === index
           );
         });
+        this.verificarOrdenesValidadas();
       },
       (error) => {
         console.error('Error al obtener la lista de órdenes detalle:', error);
       }
     );
   }
-  
-  
-  tieneTodosLosExamenesConResultados(idOrden: number): boolean {
-    return !this.ordenesConExamenesPendientes.includes(idOrden);
-  }
 
-
-  verificarResultados(): void {
-    if (this.idOrdenSeleccionada) {
+  verificarOrdenesValidadas(): void {
+    if (this.idOrdenSeleccionada !== null) {
       this.resultadosService.getResultadosByOrden(this.idOrdenSeleccionada).subscribe(
         (resultados: Resultados[]) => {
-          const examenesOrden = this.ordenesDetalle.filter((ordenDetalle) => ordenDetalle.idOrden === this.idOrdenSeleccionada);
-
-          // Verificar si todos los exámenes de la orden tienen resultados
-          this.examanesSinResultado = examenesOrden.some((ordenDetalle) =>
-            !resultados.some((resultado) => resultado.idExamen === ordenDetalle.idExamen)
+          const examenesOrden = this.ordenesDetalle.filter(
+            (ordenDetalle) => ordenDetalle.idOrden === this.idOrdenSeleccionada
           );
+  
+          const examenesValidados = examenesOrden.every((ordenDetalle) =>
+            resultados.some(
+              (resultado) =>
+                resultado.idExamen === ordenDetalle.idExamen && resultado.validado === '1'
+            )
+          );
+  
+          // Actualizar las variables mostrarBotonValidar y mostrarBotonCancelar
+          this.mostrarBotonValidar = !examenesValidados;
+          this.mostrarBotonCancelar = examenesValidados;
         },
         (error) => {
           console.error('Error al obtener los resultados de la orden:', error);
         }
       );
     } else {
-      this.examanesSinResultado = false;
+      this.mostrarBotonValidar = false;
+      this.mostrarBotonCancelar = false;
     }
   }
-
-  cancelarValidacion(): void {
-    this.mostrarFormularioValidacion = false;
-    this.fechaSeleccionada = null;
-    this.idUsuarioSeleccionado = null;
+  
+  
+  
+  ngAfterViewInit(): void {
+    this.verificarOrdenesValidadas();
   }
 
-  guardarValidacion(): void {
-    // Lógica para guardar la validación (implementar según tus necesidades)
-    console.log('Guardar validación');
+
+  esValidado(idOrden: number): boolean {
+    const resultadosOrden = this.resultados.filter(
+      (resultado) => resultado.idOrden === idOrden
+    );
+
+    return resultadosOrden.some((resultado) => resultado.validado === '1');
+  }
+
+  cancelarValidacion(idOrden: number): void {
+    this.resultadosService.getResultadosByOrden(idOrden).subscribe(
+      (resultados: Resultados[]) => {
+        const updateResults$ = resultados.map((resultado) => {
+          const updatedResultado = { ...resultado, validado: '0' };
+          return this.resultadosService.actualizarResultado(resultado.idResultados, updatedResultado);
+        });
+  
+        forkJoin(updateResults$).subscribe(
+          () => {
+            console.log('Resultados validación cancelada correctamente');
+            // Realizar cualquier otra acción necesaria después de la cancelación de la validación
+            this.mostrarFormularioValidacion = false;
+  
+            // Volver a verificar la validación cancelada
+            this.verificarValidacionCancelada();
+          },
+          (error) => {
+            console.error('Error al cancelar la validación de los resultados:', error);
+          }
+        );
+      },
+      (error) => {
+        console.error('Error al obtener los resultados de la orden:', error);
+      }
+    );
+  }
+  validarOrden(idOrden: number): void {
+    this.resultadosService.getResultadosByOrden(idOrden).subscribe(
+      (resultados: Resultados[]) => {
+        const updateResults$ = resultados.map((resultado) => {
+          const updatedResultado = { ...resultado, validado: '1' };
+          return this.resultadosService.actualizarResultado(resultado.idResultados, updatedResultado);
+        });
+  
+        forkJoin(updateResults$).subscribe(
+          () => {
+            console.log('Resultados validados correctamente');
+            // Realizar cualquier otra acción necesaria después de la validación
+            this.mostrarFormularioValidacion = true;
+            this.verificarOrdenesValidadas(); // Actualizar el estado de los botones después de la validación
+          },
+          (error) => {
+            console.error('Error al validar los resultados:', error);
+          }
+        );
+      },
+      (error) => {
+        console.error('Error al obtener los resultados de la orden:', error);
+      }
+    );
+  }
+
+  tieneTodosLosExamenesConResultados(idOrden: number): boolean {
+    return !this.ordenesConExamenesPendientes.includes(idOrden);
+  }
+
+  verificarValidacionCancelada(): void {
+    if (this.resultados.length > 0) {
+      this.validacionCancelada = this.resultados.some((resultado) => resultado.validado === '0');
+    }
   }
 }
