0c8a9992b3a60bcf9fb1c689c3b59630e4e1f851
Sergio Ayerdis <72574282+SJAR03@users.noreply.github.com>
2023-06-06 01:31:36 -0600

    diseño validar

--- src/app/shared/components/validate-button/validate-button.component.ts
+++ src/app/shared/components/validate-button/validate-button.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Usuario } from 'src/app/resultados/interfaces/results';
+import { ResultadosService } from 'src/app/resultados/services/resultados.service';
 
 @Component({
   selector: 'shared-validate-button',
@@ -6,5 +8,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./validate-button.component.css']
 })
 export class ValidateButtonComponent {
+  public mostrarFormularioValidacion: boolean = false;
+  public fechaSeleccionada: string | null = null;
+  public idUsuarioSeleccionado: string | null = null;
+  public usuarios: Usuario[] = [];
 
+  constructor(private resultadosService: ResultadosService) {}
+
+  ngOnInit(): void {
+    this.listarUsuarios();
+  }
+
+  listarUsuarios(): void {
+    this.resultadosService.listarUsuarios().subscribe(
+      (usuarios: Usuario[]) => {
+        this.usuarios = usuarios;
+      },
+      (error) => {
+        console.error('Error al obtener la lista de usuarios:', error);
+      }
+    );
+  }
+
+  cancelarValidacion(): void {
+    this.mostrarFormularioValidacion = false;
+    this.fechaSeleccionada = null;
+    this.idUsuarioSeleccionado = null;
+  }
+
+  guardarValidacion(): void {
+    // Lógica para guardar la validación (implementar según tus necesidades)
+    console.log('Guardar validación');
+  }
 }
